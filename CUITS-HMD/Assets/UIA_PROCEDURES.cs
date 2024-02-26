763806711f0a60728010b6dab9591dc69b0af697
moisesmata <moisesmata2627@gmail.com>
2024-02-25 21:24:57 -0500

    changed scene structure + UIA start
    make UIA script that turns off some highlights at start + starting to integrate TSS into hand menu

---
+++ CUITS-HMD/Assets/UIA_PROCEDURES.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIASCRIPT : MonoBehaviour
+{
+    public GameObject EMU1_POWER;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        EMU1_POWER.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
