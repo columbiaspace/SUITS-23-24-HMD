49e318f5c6302f07409c506ea601c2ce8eaa6c29
angelalbai <113465224+angelalbai@users.noreply.github.com>
2024-05-19 22:19:32 -0500

    maybe mini map works?

---
+++ CUITS-HMD/Assets/Scripts/convert_coords.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class convert_coords : MonoBehaviour
+{
+    public GameObject pin;
+    public TSS_DATA TSS;
+
+    double uniposx;
+    double uniposy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uniposx = TSS.imu.imu.eva1.posx - 298354.99707 + (.0030824 * (TSS.imu.imu.eva1.posx - 298355));
+        uniposy = TSS.imu.imu.eva1.posy - 3272382.95751 + (.0026632407 * (TSS.imu.imu.eva1.posy - 3272383));
+
+        pin.transform.position = new Vector3((float)uniposx, (float)uniposy, 0);
+    }
+
+}
